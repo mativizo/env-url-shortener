f8b5d0d69758b620c15f5aea7f3d8857c9ab481b
Mativizo <mativizo@outlook.com>
2023-05-06 09:38:28 +0200

    one-line, rewrite, fastify id in url.

--- index.js
+++ index.js
@@ -1,82 +1,135 @@
 // Requires
-const dotenv = require('dotenv')
 const fastify = require('fastify')({ logger: false })
 
 // check if it's test env
 const isDevEnv = process.env.npm_lifecycle_event === 'dev'
 
 // if is not a production environment
-if (isDevEnv) dotenv.config()
+if (isDevEnv) {
+    const dotenv = require('dotenv')
+    dotenv.config()
+}
 
-// get port
-const port = process.env.PORT || 3000
+function getConfig() {
+    return {
+        PORT: process.env.PORT || 8080,
+        PREFIX: process.env.VAR_PREFIX || 'URL_',
+        IS_ONE_LINE_SYNTAX: process.env.IS_ONE_LINE_SYNTAX == 1,
+        DEBUG: process.env.DEBUG == 1,
+        ONE_LINE_DELIMITER: process.env.ONE_LINE_DELIMITER || '|',
+    }
+}
 
-function getUrlsFromEnv(prefix) {
-    
+function getUrlsFromEnv(config) {
+
     // Get all env keys
     const envKeys = Object.keys(process.env)
 
-    // Create empty object
+    // Create empty objects
     const urls = {}
+    const temp = {}
 
     // loop through all env variables
     for (let i = 0; i < envKeys.length; i++) {
         const key = envKeys[i]
+        
         const value = process.env[key]
-        let name = key.replace(prefix, '')
+        if (key.startsWith(config.PREFIX)) {
+            
+            let name = key.replace(config.PREFIX, '')
 
-        let url = value;
-        let lowerCaseUrl = url.toLowerCase();
-        if (!lowerCaseUrl.startsWith("https://") && !lowerCaseUrl.startsWith("http://")) {
-            url = `https://${url}`
-        }
+            if (config.IS_ONE_LINE_SYNTAX !== true) {
+                let url = value;
+                if (key.endsWith('_GO')) {
+                    name = name.replace('_GO', '')
+                    if (!temp[name]) urls[name] = {}
+                    temp[name] = { ...temp[name], go: url }
+                } else if (key.endsWith('_ID')) {
+                    name = name.replace('_ID', '')
+                    if (!temp[name]) temp[name] = {}
+                    temp[name] = { ...temp[name], id: value }
+                }
 
-        if (key.endsWith('_GO')) {
-            name = name.replace('_GO', '')
-            if (!urls[name]) urls[name] = {}
-            urls[name] = {
-                ...urls[name],
-                go: url
+                const tempKeys = Object.keys(temp)
+                for (tempKey of tempKeys) {
+                    const item = temp[tempKey]
+                    if (item.id && item.go) {
+                        urls[item.id] = item.go
+                    }
+                }
+
+            } else {
+
+                const ids = value.split(config.ONE_LINE_DELIMITER)
+                let url = ids.pop()
+
+                for (id of ids) {
+                    urls[id] = url
+                }
             }
-        } else if (key.endsWith('_ID')) {
-            name = name.replace('_ID', '')
-            if (!urls[name]) urls[name] = {}
-            urls[name] = {
-                ...urls[name],
-                id: value
-            }
+
         }
     }
-    
+
     return urls
+}
+
+function formatUrl(url) {
+    if (!url.toLowerCase().startsWith("https://") && !url.toLowerCase().startsWith("http://")) {
+        url = `https://${url}`
+    }
+    return url
 }
 
 
 function main() {
+    // load config
+    const config = getConfig()
 
-    // get default variables prefix
-    const prefix = process.env.VAR_PREFIX || 'URL_';
+    // get urls
+    const urls = getUrlsFromEnv(config)
 
-    // get urls from env
-    const urls = getUrlsFromEnv(prefix);
-
-    // create routes for urls
-    for (let i = 0; i < Object.keys(urls).length; i++) {
-        const key = Object.keys(urls)[i]
-        const value = urls[key]
-        
-        let url = `/${value.id}`
-        if (value.id == "*") url = `/`
-        
-        fastify.get(url, async (request, reply) => {
-            reply.redirect(value.go)
-        })
+    if (config.DEBUG) {
+        console.log(config);
+        console.log(urls)
     }
 
-    // serve
-    fastify.listen({port}, (err, address) => {
+    fastify.get('/', async (request, reply) => {
+        if (urls["*"]) {
+            reply.redirect(formatUrl(urls["*"].go))
+        } else {
+            reply.code(404).send({
+                message: 'Not found'
+            })
+        }
+    })
+
+    fastify.get("/:id", async (request, reply) => {
+        const id = request.params.id
+        
+        if (id === 'debug' && config.DEBUG) {
+            reply.code(200).send({
+                message: JSON.stringify({
+                    config, urls
+                })
+            })
+            return
+        }
+        
+        if (urls[id]) {
+            reply.redirect(formatUrl(urls[id]))
+            return
+        }
+        reply.code(404).send({
+            message: 'Not found'
+        })
+        return
+    })
+
+    // start server
+    fastify.listen({ port: config.PORT }, (err, address) => {
         if (err) throw err
-        console.log(`Server listening on ${port}`)
+        console.log(`Server listening on ${config.PORT}`)
     })
 }
 
